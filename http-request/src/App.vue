9d85a5cabe7b86d582890c71c604e90370cebedd
Gamzenur Demir <gamzenurdemir3469@gmail.com>
2022-08-19 23:19:30 +0300

    first commit

---
+++ http-request/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h3>Alışveriş Listesi</h3>
+    <hr />
+    <div class="my-2">
+      <input type="text" placeholder="ne alacaksın?" @keydown.enter="onSave" />
+    </div>
+    <ul>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+    </ul>
+    <small class="mt-2 text-red d-flex justify-content-end align-items-center"
+      >5 adet alınacak ürün vardır ...</small
+    >
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      itemsList: [],
+    };
+  },
+
+  methods: {
+    onSave(e) {
+      const saveObject = {
+        title: e.target.value,
+        created_at: new Date(),
+        completed: false,
+      };
+      axios
+        .post("http://localhost:3000/items", saveObject)
+        .then((save_response) => {
+          console.log(save_response);
+          this.itemsList.push(save_response.data);
+          e.target.value = "";
+          e.target.focus();
+        });
+    },
+  },
+};
+</script>
